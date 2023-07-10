03a9031fccb84518d7993dd3d00fd1167e7dcf14
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2023-07-10 20:49:11 +0300

    feed pagination

--- src/modules/feed/api/repository.ts
+++ src/modules/feed/api/repository.ts
@@ -2,15 +2,24 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { axiosBaseQuery } from '../../../core/axios-base-query'
 import { GlobalFeedInDTO } from './dto/global-feed.in';
+import { FEED_PAGE_SIZE } from '../consts';
+
+interface GlobalFeedParams {
+  page: number;
+}
 
 export const feedApi = createApi({
   reducerPath: 'feedApi',
   baseQuery: axiosBaseQuery({ baseUrl: 'https://api.realworld.io/api/' }),
   endpoints: (builder) => ({
-    getGlobalFeed: builder.query<GlobalFeedInDTO, any>({
-      query: () => ({
+    getGlobalFeed: builder.query<GlobalFeedInDTO, GlobalFeedParams>({
+      query: ({ page }) => ({
         url: '/articles',
         method: 'get',
+        params: {
+          limit: FEED_PAGE_SIZE,
+          offset: page + FEED_PAGE_SIZE,
+        }
       }),
     }),
   }),
--- src/modules/feed/components/feed/feed.component.tsx
+++ src/modules/feed/components/feed/feed.component.tsx
@@ -1,14 +1,25 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 import { ArticleList, FeedToggle } from '../index';
 import { Container } from '../../../../common/components';
 import { useGetGlobalFeedQuery } from '../../api/repository';
+import { FEED_PAGE_SIZE } from '../../consts';
+import { useSearchParams } from 'react-router-dom';
+import { serializeSearchParams } from '../../../../utils/router';
 
 interface FeedProps {}
 
 export const Feed: FC<FeedProps> = () => {
-  const { data, error, isLoading } = useGetGlobalFeedQuery('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [page, setPage] = useState(searchParams.get('page') ? Number(searchParams.get('page')) : 0);
+  const onHandlePageChange = ({selected}: {selected: number}) => {
+    setPage(selected);
+    setSearchParams(serializeSearchParams({ page: String(selected) }));
+  }
 
-  if(isLoading){
+  const { data, error, isLoading, isFetching } = useGetGlobalFeedQuery({ page });
+
+  if(isLoading || isFetching){
     return (
       <Container>
         Feed is loading...
@@ -28,7 +39,25 @@ export const Feed: FC<FeedProps> = () => {
     <Container>
       <FeedToggle />
       <div className='flex'>
-        <ArticleList list={data?.articles || []} />
+        <div className="w-3/4">
+          <ArticleList list={data?.articles || []} />
+          <nav className="my-8">
+            <ReactPaginate
+              pageCount={(data?.articlesCount || 0) / FEED_PAGE_SIZE} 
+              pageRangeDisplayed={(data?.articlesCount || 0) / FEED_PAGE_SIZE} 
+              previousLabel={null}
+              nextLabel={null}
+              containerClassName='flex'
+              pageClassName='group'
+              pageLinkClassName='p-3 text-conduit-green bg-white border border-conduit-lightenGray -ml-px 
+                hover:bg-conduit-pageHoverBg group-[&:nth-child(2)]:rounded-l group-[&:nth-child(2)]:rounded-r'
+              activeClassName='active group'
+              activeLinkClassName='group-[.active]:bg-conduit-green group-[.active]:text-white group-[.active]:border-conduit-green'
+              onPageChange={onHandlePageChange}
+              forcePage={page}
+            />
+          </nav>
+        </div>
         <div className='w-1/4'>tags</div>
       </div>
     </Container>
